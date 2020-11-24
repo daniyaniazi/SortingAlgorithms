ef1b5601c482446c526febedd2098f908fafc8fe
daniyaniazi <farihadania@hotmail.com>
2020-11-24 21:33:59 +0500

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void merge(int array[], int mid, int start, int end)
+void mergeSort(int array[], int mid, int start, int end)
 {
     int i, j, k,temparr[5];
     i = start;
@@ -35,30 +35,30 @@ void merge(int array[], int mid, int start, int end)
         k++;
         j++;
     }
-    // New Array
+    // Sorted Array
     for (int i = start; i <= end; i++) 
     {
         array[i] = temparr[i];
     }
 }
 
-void mergeSort(int array[], int start, int end){
+void mergeBreak(int array[], int start, int end){
     int mid; 
     if(start<end){
     
     	// spliting an array  in to 2 parts
         mid = (start + end) /2;
         // Breaking Array into smaller part untill no operation to be perform left
-        mergeSort(array, start, mid); //Left part
-        mergeSort(array, mid+1, end); //right part
+        mergeBreak(array, start, mid); //Left part
+        mergeBreak(array, mid+1, end); //right part
         
-        merge(array, mid, start, end);
+        mergeSort(array, mid, start, end);
         	
     }
 }
 int main(){
 	int arr[] = {2, 6, 7, 3, 1};
-	mergeSort(arr, 0, 4);
+	mergeBreak(arr, 0, 4);
  	cout<< "After Sorting ";
 	for (int i =0 ; i<5;i++){
      	cout<<arr[i]<<" ";
